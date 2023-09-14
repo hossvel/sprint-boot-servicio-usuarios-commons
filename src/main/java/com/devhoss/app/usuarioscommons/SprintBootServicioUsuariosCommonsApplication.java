5e80ea99358ae47aeb972bf3eb084673b34b2f2d
devhoss <velasco2288@gmail.com>
2023-09-13 23:18:15 -0500

    first commit

---
+++ src/main/java/com/devhoss/app/usuarioscommons/SprintBootServicioUsuariosCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.devhoss.app.usuarioscommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootServicioUsuariosCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootServicioUsuariosCommonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devhoss/app/usuarioscommons/SprintBootServicioUsuariosCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhoss.app.usuarioscommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootServicioUsuariosCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
